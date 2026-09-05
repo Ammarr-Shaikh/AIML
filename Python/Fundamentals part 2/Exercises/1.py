a1ddf2ba30dbc939e89388e1176007e6c32eacea
Ammar <skammar0506@gmail.com>
2026-09-05 18:15:52 +0530

    Remove old Exercises folder

--- Python/Fundamentals part 2/Exercises/1.py
+++
@@ -1,9 +0,0 @@
-age = int(input("Enter your age: "))
-if age < 13:
-    print("Child.")
-elif age >= 13 and age <= 18:
-    print("Teenager")
-elif age > 18:
-    print("Adult.")
-else:
-    print("Invalid input")
