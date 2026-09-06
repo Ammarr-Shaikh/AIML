0d0f6b9bc730476b812398f49013a84310563ba7
Ammar <skammar0506@gmail.com>
2026-09-06 23:57:02 +0530

    improving

--- Python/Fundamentals part 2/2.py
+++ Python/Fundamentals part 2/2.py
@@ -1,11 +1,13 @@
-
 while True:
-  
- 
-            num = int(input("Enter a number: "))
-            if num < 0:
-                print("it is negative")
-            elif num == 0:
-                print("Number is zero")
-            else:
-                print("It is positive")
+
+    user = input("Do you wanna continue")
+    if user =="quit":
+        break
+    else:
+        n = int(input("Enter a number: "))
+        if n > 0:
+            print("it is positive")
+        elif n <0:
+            print("it is negative")
+        else:
+            print("number is zero")
