aaa76cd476ebb7b7ff2a98e5880cf1c044396173
Ammar <skammar0506@gmail.com>
2026-09-06 23:50:46 +0530

    error

---
+++ Python/Fundamentals part 2/2.py
@@ -0,0 +1,11 @@
+
+while True:
+  
+ 
+            num = int(input("Enter a number: "))
+            if num < 0:
+                print("it is negative")
+            elif num == 0:
+                print("Number is zero")
+            else:
+                print("It is positive")
